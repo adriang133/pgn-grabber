05e191761935da59906f666c32767a410236363f
adriang133 <16702919+adriang133@users.noreply.github.com>
2018-02-17 16:52:34 +0100

    tabs->spaces

--- src/pgngrabber.js
+++ src/pgngrabber.js
@@ -1,47 +1,47 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+    var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+        clearInterval(readyStateCheckInterval);
 
-		var button = document.querySelector("a[data-bind*='urlPGN']");
-		if(button){
-			button.setAttribute("href", "#");
-			button.style.backgroundColor = "green";
-			button.addEventListener("click", (event) => { 
-				event.preventDefault(); 
-				downloadPgn(); 
-			}, true);
-		}
+        var button = document.querySelector("a[data-bind*='urlPGN']");
+        if(button){
+            button.setAttribute("href", "#");
+            button.style.backgroundColor = "green";
+            button.addEventListener("click", (event) => { 
+                event.preventDefault(); 
+                downloadPgn(); 
+            }, true);
+        }
 
-	}
-	}, 10);
+    }
+    }, 10);
 });
 
 function downloadPgn(){
-	chrome.runtime.sendMessage(pgn());
+    chrome.runtime.sendMessage(pgn());
 }
 
 function pgn(){
-	var gameRoot = document.querySelector("div[data-bind*='tmpl-move-viewer-moves']");
-	var gameMoves = gameRoot.querySelector("span[data-bind*='tmpl-mv-variation']");
-	var result = gameRoot.querySelector("span[class*='cbMoveResult']").textContent;
+    var gameRoot = document.querySelector("div[data-bind*='tmpl-move-viewer-moves']");
+    var gameMoves = gameRoot.querySelector("span[data-bind*='tmpl-mv-variation']");
+    var result = gameRoot.querySelector("span[class*='cbMoveResult']").textContent;
 
-	var moves = ["dummy"];
+    var moves = ["dummy"];
 
-	for(var moveNode of gameMoves.children){
-		var moveText = moveNode.querySelector("span[class*='cbRootMove']").textContent;
-		moves.push(moveText);
-	}
+    for(var moveNode of gameMoves.children){
+        var moveText = moveNode.querySelector("span[class*='cbRootMove']").textContent;
+        moves.push(moveText);
+    }
 
-	var gameInfo = document.querySelector("div[data-bind*='tmpl-game-info']");
+    var gameInfo = document.querySelector("div[data-bind*='tmpl-game-info']");
 
-	var whitePlayer = gameInfo.querySelector("a[data-bind*='white()']").textContent;
-	var blackPlayer = gameInfo.querySelector("a[data-bind*='black()']").textContent;
+    var whitePlayer = gameInfo.querySelector("a[data-bind*='white()']").textContent;
+    var blackPlayer = gameInfo.querySelector("a[data-bind*='black()']").textContent;
 
-	var eventName = gameInfo.querySelector("a[data-bind*='data().roomData.name']").textContent;
-	var round = gameInfo.querySelector("span[data-bind*='data().round']").textContent;
+    var eventName = gameInfo.querySelector("a[data-bind*='data().roomData.name']").textContent;
+    var round = gameInfo.querySelector("span[data-bind*='data().round']").textContent;
 
-	var pgn = `
+    var pgn = `
 ${pgnTag("Event", eventName)}
 ${pgnTag("Round", round)}
 ${pgnTag("White", whitePlayer)}
@@ -51,24 +51,24 @@ ${pgnTag("Result", result)}
 ${moveListString(moves)}
 `;
 
-	return pgn;
+    return pgn;
 }
 
 function pgnTag(key, value){
-	return `[${key} "${value}"]`;
+    return `[${key} "${value}"]`;
 }
 
 function moveListString(moves){
-	var result = "";
-	for(var i = 1; i < moves.length; i += 2){
-		if(i > 1){
-			result += " ";
-		}
-		result += `${(Math.floor(i / 2) + 1).toString()}. ${moves[i]}`;
-		if(i + 1 < moves.length){
-			result += ` ${moves[i + 1]}`;
-		}
-	}
+    var result = "";
+    for(var i = 1; i < moves.length; i += 2){
+        if(i > 1){
+            result += " ";
+        }
+        result += `${(Math.floor(i / 2) + 1).toString()}. ${moves[i]}`;
+        if(i + 1 < moves.length){
+            result += ` ${moves[i + 1]}`;
+        }
+    }
 
-	return result;
+    return result;
 }
